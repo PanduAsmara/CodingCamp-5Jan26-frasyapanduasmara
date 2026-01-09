b1cce4ff1698ab6999be2ce66527d3e08e64ab7f
Pandu Asmara <frasyapanduasmara@gmail.com>
2026-01-10 00:46:04 +0700

    variable
    mengambil elemen html di javascript

--- js/script.js
+++ js/script.js
@@ -1,1 +1,6 @@
 console.log("javascript connected");
+
+var form = document.querySelector("form");
+var inputText = document.querySelector("input[type='text']");
+var inputDate = document.querySelector("input[type='date']");
+var list = document.querySelector("ul");
